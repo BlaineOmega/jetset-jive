f64a36e96dd540346e71d003cd3c23521587f8b2
Blaine <blainevanderson@gmail.com>
2024-06-05 06:57:07 -0700

    new file for test

---
+++ app/team/page_temp.jsx
@@ -0,0 +1,30 @@
+import Team from "@/components/team/Team";
+import Sidebar from "@/components/sidebar/Sidebar";
+
+export const metadata = {
+  title: "Team",
+};
+const index = () => {
+  return (
+    <>
+      {/* En Seo Page Title */}
+
+      <Sidebar />
+      {/* END LEFT MENU CONTENT */}
+
+      {/* START RIGHT PART CONTENT */}
+      <div className="rightpart">
+        <div className="rightpart_in">
+          <div className="tokyo_tm_section">
+            <div data-aos="fade-right" data-aos-duration="1200">
+              <Team />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* END RIGHT PART CONTENT */}
+    </>
+  );
+};
+
+export default index;
