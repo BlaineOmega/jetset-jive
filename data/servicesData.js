df800475cf00914b546d303357df7bc00eee4798
Blaine <blainevanderson@gmail.com>
2024-06-05 08:12:22 -0700

    services update

--- data/servicesData.js
+++ data/servicesData.js
@@ -77,11 +77,10 @@ module.exports = [
   {
     id: 4,
     no: "04",
-    title: "Live Chat",
-    text: `Web development is the most famous job in the world and it is very
-    interesting...`,
+    title: "Paper Prototypes",
+    text: `Paper prototypes was an interesting exercise`,
     largeImg: "/img/news/4.jpg",
-    largeTitle: "Live Chat a new tool that enables direct video hosting",
+    largeTitle: "Time for arts and crafts...",
     poster: "SHIKA VAIL",
     date: "12 APRIL 2021",
     descriptions: (
@@ -112,9 +111,8 @@ module.exports = [
   {
     id: 5,
     no: "05",
-    title: "After Effects",
-    text: `Web development is the most famous job in the world and it is very
-    interesting...`,
+    title: "Wireframes",
+    text: `Building wireframes allowed us to better layout our application...`,
     largeImg: "/img/news/1.jpg",
     largeTitle: "After Effects a new tool that enables direct video hosting",
     poster: "JIJA ZORIA",
@@ -140,11 +138,80 @@ module.exports = [
   {
     id: 6,
     no: "06",
-    title: "Mobile App",
+    title: "Site map",
     text: `Web development is the most famous job in the world and it is very
     interesting...`,
     largeImg: "/img/news/2.jpg",
     largeTitle: "Mobile App a new tool that enables direct video hosting",
+    poster: "HASAN ALI",
+    date: "09 APRIL 2021",
+    descriptions: (
+      <>
+        <p>
+          Most photographers love to shoot the unusual, and you don’t get much
+          more unusual than These Unprecedented Times. Right now everything
+          counts as out of the ordinary. There are a number of remarkable things
+          about these lockdown days that are worth photographing now so we can
+          remember them when it is all over.
+        </p>
+        <p>
+          Pretend everything is new and that you haven’t seen it before, and
+          then you will be free to notice the leading lines, the places where
+          one edge meets another in delightful geometric harmony, and how the
+          ordinary things in the kitchen are transformed when the light is on or
+          off.
+        </p>
+        <p>
+          The trick here is to look slowly, and then look again. Take the time
+          to look in detail and to look at the same thing from different angles,
+          with different light, long lenses and wide lenses. Then move to the
+          left a bit. You may never feel the need to leave the house again.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 7,
+    no: "07",
+    title: "Style Guide",
+    text: `Web development is the most famous job in the world and it is very
+    interesting...`,
+    largeImg: "/img/news/2.jpg",
+    largeTitle: "Mobile App a new tool that enables direct video hosting",
+    poster: "HASAN ALI",
+    date: "09 APRIL 2021",
+    descriptions: (
+      <>
+        <p>
+          Most photographers love to shoot the unusual, and you don’t get much
+          more unusual than These Unprecedented Times. Right now everything
+          counts as out of the ordinary. There are a number of remarkable things
+          about these lockdown days that are worth photographing now so we can
+          remember them when it is all over.
+        </p>
+        <p>
+          Pretend everything is new and that you haven’t seen it before, and
+          then you will be free to notice the leading lines, the places where
+          one edge meets another in delightful geometric harmony, and how the
+          ordinary things in the kitchen are transformed when the light is on or
+          off.
+        </p>
+        <p>
+          The trick here is to look slowly, and then look again. Take the time
+          to look in detail and to look at the same thing from different angles,
+          with different light, long lenses and wide lenses. Then move to the
+          left a bit. You may never feel the need to leave the house again.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 8,
+    no: "08",
+    title: "Final product",
+    text: `The outcome of all our hard work`,
+    largeImg: "/img/news/2.jpg",
+    largeTitle: "Final product",
     poster: "HASAN ALI",
     date: "09 APRIL 2021",
     descriptions: (
